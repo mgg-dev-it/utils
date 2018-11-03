c73352c5b109261e707ef467ebbfe9c75a3f5340
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-03 20:23:17 +0100

    .

--- utils/src/main/java/mggdevit/utils/util/JavaSourceAnalyzer.java
+++ utils/src/main/java/mggdevit/utils/util/JavaSourceAnalyzer.java
@@ -1,8 +1,15 @@
 package mggdevit.utils.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Vector;
 
 /**
  *
@@ -12,11 +19,20 @@ public class JavaSourceAnalyzer {
 
     private ArrayList<String> alDirectoriesAndFiles;
     private ArrayList<String> alSourceFiles;
-    private HashMap<String,String> hmFilesAndImports;
+    private ArrayList<String> alTopFiles;
+    private HashSet<String> hsImportedFiles;
+    private ArrayList<String> alOtherFiles;
+    private HashMap<String, String> hmFilesAndImportsOld;
+    private HashMap<String, ArrayList<String>> hmFilesAndImports;
 
     public JavaSourceAnalyzer() {
         alDirectoriesAndFiles = new ArrayList<>();
         alSourceFiles = new ArrayList<>();
+        alTopFiles = new ArrayList<>();
+        hsImportedFiles = new HashSet<>();
+        alOtherFiles = new ArrayList<>();
+        hmFilesAndImportsOld = new HashMap<>();
+        hmFilesAndImports = new HashMap<>();
     }
 
     public void addDirectoryOrFile(String sFullName) {
@@ -39,9 +55,53 @@ public class JavaSourceAnalyzer {
         //alSourceFiles.sort((c1,c2) ->(c1.compareTo(c2)));
         alSourceFiles.sort(String::compareTo);
         for (int i = 0; i < alSourceFiles.size(); i++) {
-            System.out.println(alSourceFiles.get(i));
+            //System.out.println(alSourceFiles.get(i));
+            processFile(alSourceFiles.get(i), hmFilesAndImports);
+        }
+        //System.out.println("-----");
+        //System.out.println(hmFilesAndImports.size() + " top level Java source file was found!");
+        //hmFilesAndImports.forEach((k, v) -> System.out.println("file: " + k + " import:" + v));
+//        hmFilesAndImportsOld.forEach((k, v) -> addToTop(k, v));
+        alTopFiles.sort(String::compareTo);
+        hmFilesAndImports.forEach((k, v) -> alOtherFiles.add(k));
+        alOtherFiles.sort(String::compareTo);
+        System.out.println("-- " + alTopFiles.size() + " top level files (do not use import)");
+//        alTopFiles.forEach(s -> System.out.println(s));
+//        alTopFiles.forEach(s -> System.out.println(s + (isImportedFile(s) ? " imported" : "")));
+        alTopFiles.forEach(s -> System.out.println(s + importInfo(s)));
+//        alTopFiles.forEach(s -> hmFilesAndImportsOld.remove(s));
+        System.out.println("-- other files");
+//        hmFilesAndImportsOld.forEach((k, v) -> System.out.println("file: " + k + " import:" + v));
+
+        for (int i = 0; i < alOtherFiles.size(); i++) {
+            ArrayList<String> als = hmFilesAndImports.get(alOtherFiles.get(i));
+            als.sort(String::compareTo);
+            for (int j = 0; j < als.size(); j++) {
+                System.out.println(alOtherFiles.get(i) + " " + als.get(j));
+            }
         }
     }
+
+    private String importInfo(String sName) {
+        String sRetVal = "";
+        for (int i = 0; i < alOtherFiles.size(); i++) {
+            ArrayList<String> als = hmFilesAndImports.get(alOtherFiles.get(i));
+            if (als.contains(sName)) {
+                sRetVal += (sRetVal.length() == 0 ? "" : ", ") + alOtherFiles.get(i);
+            }
+        }
+        return ((sRetVal.length() == 0 ? "" : " used by: [") + sRetVal + (sRetVal.length() == 0 ? "" : "]"));
+    }
+
+//    private boolean isImportedFile(String sName) {
+//        return (hsImportedFiles.contains(sName));
+//    }
+
+//    private void addToTop(String sFileName, String sImport) {
+//        if (sImport.length() == 0) {
+//            alTopFiles.add(sFileName);
+//        }
+//    }
 
     private void collectSourceFiles(String sFullName, ArrayList<String> alSourceFiles) {
         File f = new File(sFullName);
@@ -78,6 +138,60 @@ public class JavaSourceAnalyzer {
                 }
             }
         }
+    }
+
+    private void processFile(String sSourceFileName, HashMap<String, ArrayList<String>> hmFilesAndImports) {
+        String sFileName = sSourceFileName.substring(sSourceFileName.lastIndexOf(File.separator) + 1, sSourceFileName.length() - ".java".length());
+        Vector<String> vLines = readTextFileIntoStringVector(sSourceFileName);
+        String sPackage = "";
+        int iImportCount = 0;
+        ArrayList<String> alImportList = new ArrayList<>();
+        for (int iLine = 0; iLine < vLines.size(); iLine++) {
+            String sLine = vLines.elementAt(iLine).trim();
+            if (sLine.startsWith("package ")) {
+                sPackage = sLine.substring("package ".length(), sLine.length() - 1);
+            }
+            if (sLine.startsWith("import ")) {
+                String sImport = sLine.substring("import ".length(), sLine.length() - 1);
+                if (sImport.startsWith("hu")) {
+//                    hmFilesAndImports.put(sPackage + "." + sFileName, sImport);
+                    alImportList.add(sImport);
+                    ++iImportCount;
+                    hsImportedFiles.add(sImport);
+                }
+            }
+        }
+        if (iImportCount == 0) {
+//            hmFilesAndImports.put(sPackage + "." + sFileName, "");
+            alTopFiles.add(sPackage + "." + sFileName);
+        } else {
+            hmFilesAndImports.put(sPackage + "." + sFileName, alImportList);
+        }
+        //System.out.println(sSourceFileName + " " + sPackage + " " + sFileName);
+    }
+
+    public static Vector<String> readTextFileIntoStringVector(String sFileName) {
+        File file = null;
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        String sLine = null;
+        Vector<String> vLines = new Vector<String>();
+
+        try {
+            file = new File(sFileName);
+            fis = new FileInputStream(file);
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            while ((sLine = br.readLine()) != null) {
+                vLines.add(sLine);
+            }
+        } catch (FileNotFoundException fnfe) {
+//            appInterface.handleError(fnfe);
+        } catch (IOException ioe) {
+//            appInterface.handleError(ioe);
+        }
+        return (vLines);
     }
 
 }
